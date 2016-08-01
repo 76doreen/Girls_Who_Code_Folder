3a1115b1b71bd7cbfb87f12a04dcc8d059393ae4
Doreen Wu <76doreen@gmail.com>
2016-08-01 14:12:02 -0400

    updating changes on my runner game

--- runner_game.py
+++ runner_game.py
@@ -1,8 +1,7 @@
 import pygame
 from random import *
 from block import *
-from building import *
-
+from building1 import *
 
 pygame.init()
 screen_width = 700
@@ -24,8 +23,9 @@ AZURE = (240, 255, 255)
 ALICE_BLUE = (240, 248, 255)
 SALMON = (250, 128, 114)
 LIGHT_SALMON = (255, 165, 0)
+LIGHT_CORAL = (240, 128, 128)
 
-colors = [BLACK, WHITE, GREEN, BLUE, RED, GREY, LAVENDER, LAVENDER_BLUSH, MISTY_ROSE, HONEYDEW, MINT_CREAM, ALICE_BLUE, SALMON, LIGHT_SALMON]
+colors = [BLACK, WHITE, GREEN, BLUE, RED, GREY, LAVENDER, LAVENDER_BLUSH, MISTY_ROSE, HONEYDEW, MINT_CREAM, AZURE, ALICE_BLUE, SALMON, LIGHT_SALMON, LIGHT_CORAL]
 
 clock = pygame.time.Clock()
 score = 0
@@ -44,21 +44,23 @@ while not done:
         if event.type == pygame.QUIT: 
             done = True
 
-    if lives <= 0:    
-        screen.fill(RED)
-        text = font1.render("GAME OVER.", True, WHITE)
-        screen.blit(text, [120, 250])
-    else:
-        screen.fill(AZURE)
+    picture = pygame.image.load('DonaldTrump.jpg')
+    screen.blit(picture, (350, 350))
+    
+    buildings_list = []
 
-        buildings_list = []
+    x_point = 700
+    y_point = 0
+    width = random.randint(50, 75)
+    height = random.randint(100, 500)
+    building_color = random.choice(colors)
 
-        building = Building(random.choice(colors), random.randint(100,800), random.randint(25, 50), 800, random.randint(100, 600))
-        buildings_list.append(building)
+    building = Building(700 + width, 500 - height, width, 600, random.choice(colors))
+    buildings_list.append(building)
 
-        for building in buildings_list:
-            building.draw_building()
-            building.move_building()
+    for building in buildings_list:
+        building.draw()
+        building.move(2)
 
     pos = pygame.mouse.get_pos()
 
@@ -83,6 +85,22 @@ while not done:
     score_text = font.render("Score: " +str(score), True, BLACK)
     lives_text = font.render("Lives: "+ str(lives), True, BLACK)
 
+    if lives <= 0:    
+        screen.fill(RED)
+        text = font1.render("GAME OVER.", True, WHITE)
+        screen.blit(text, [120, 250])
+    else:
+        screen.fill(AZURE)
+
+    if score >= 15 and score < 30:
+        screen.fill(LAVENDER)
+
+    if score >=30 and score <45:
+        screen.fill(LIGHT_CORAL)
+
+
+    if score >=45:
+        screen.fill(MISTY_ROSE)
 
     screen.blit(score_text, [500, 50])
     screen.blit(lives_text, [50, 50])
@@ -90,6 +108,10 @@ while not done:
     all_sprites_list.draw(screen)
 
     pygame.display.flip()
+
+    if lives <= 0: 
+        pygame.time.delay(1000)
+        done = True
 
     clock.tick(60)
 
